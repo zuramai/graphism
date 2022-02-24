6eb3599c5d3ba7598ac3441ac3915b4be922edd3
zuramai <saugi.dev@gmail.com>
2022-02-24 09:15:41 +0700

    add: notification

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,34 +1,40 @@
 import './assets/scss/main.scss'
 import { createGraphism } from "../../packages/graphism/src"
 import { NodeInterface } from '../../packages/graphism/src/types'
+import { Graphism } from 'graphism'
 
-const el = document.querySelector<HTMLCanvasElement>('#canvas')
+window.onload = () => {
+    const el = document.querySelector<HTMLCanvasElement>('#canvas')
 
-// Create the canvas instance
-const canvas = createGraphism({
-    el,
-    canvasBackground: "#eee",
-})
+    // Create the canvas instance
+    const graphism = createGraphism({
+        el,
+        canvasBackground: "#fafafa",
+    })
+    
+    
+    // Event listeners
+    document.getElementById('node-add').addEventListener('click', addNode.bind(null, graphism))
+    document.getElementById('generate-graph').addEventListener('click', generateGraphEvent.bind(null, graphism))
+    document.getElementById('create-new').addEventListener('click', createNewGraph)
+    document.getElementById('connectNode').addEventListener('click', connectNode.bind(null, graphism))
+    
+}
 
-const helperText = <HTMLElement>document.getElementById('helper-text')
-
-// Event listeners
-document.getElementById('node-add').addEventListener('click', addNode)
-document.getElementById('generate-graph').addEventListener('click', generateGraphEvent)
-document.getElementById('create-new').addEventListener('click', createNewGraph)
-document.getElementById('connectNode').addEventListener('click', connectNode)
 
 /**
  * Execute create new node
  */
-function addNode() {
+function addNode(graphism: Graphism) {
     let name = <HTMLFormElement>document.getElementById("name")
     document.querySelector('.modal-add').classList.remove('modal-open')
 
     showHelperText(`Click anywhere to create ${name.value} node`)
 
-    canvas.waitingForClick().then(coordinate => {
-        canvas.createNode(name ? name.value : "", coordinate)
+    graphism.waitingForClick().then(coordinate => {
+        let nameVal = name ? name.value : ""
+        graphism.createNode(nameVal, coordinate)
+        createNotification("success", `Node ${nameVal} created`)
         hideHelperText()
     })
 }
@@ -37,22 +43,21 @@ function addNode() {
 /**
  * Start connecting mode in canvas
  */
-function connectNode() {
-    canvas.setMode('connecting')
+function connectNode(graphism: Graphism) {
+    graphism.setMode('connecting')
     showHelperText("Click any node to connect")
 
-    canvas.on("node:connect", (node1: NodeInterface, node2: NodeInterface) => {
+    graphism.on("node:connect", (node1: NodeInterface, node2: NodeInterface) => {
         hideHelperText()
     }) 
-
 }
 
 /**
  * Create new and start with random graph
  */
-function generateGraphEvent() {
-    const nodes = canvas.generateGraph()
-    canvas.nodes = nodes
+function generateGraphEvent(graphism: Graphism) {
+    const nodes = graphism.generateGraph()
+    graphism.nodes = nodes
     hideTitleScreen()
 }
 
@@ -71,6 +76,8 @@ function hideTitleScreen() {
     let titleScreen = document.querySelector<HTMLElement>('.page-title')
     titleScreen.classList.toggle('show')
 }
+
+const helperText = <HTMLElement>document.getElementById('helper-text')
 
 /**
  * Show a helper text with custom text
@@ -91,4 +98,38 @@ function showHelperText(text: string, blink: boolean = true) {
 function hideHelperText() {
     helperText.classList.remove('blinking')
     helperText.classList.remove('show')
+}
+
+/**
+ * 
+ * @param type The notification type (primary, danger, warning)
+ * @param text Text content to be displayed in notification
+ * @param duration The duration of notification to stay visible
+ */
+function createNotification(type: string, text: string, duration: number =  2000) {
+    let notif = document.createElement('div')
+    notif.classList.add("notification")
+    notif.classList.add(`notification-${type}`)
+    notif.innerText = text
+
+    let btn = document.createElement('button')
+    let svgClose = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    let path = document.createElementNS('http://www.w3.org/2000/svg', "path")
+    svgClose.setAttribute('width', "16")
+    svgClose.setAttribute('height', "16")
+    path.setAttribute("d", "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z")
+    
+    
+    svgClose.appendChild(path)
+    btn.appendChild(svgClose)
+    notif.appendChild(btn)
+
+    let wrapper = document.querySelector('.notifications')
+    wrapper.appendChild(notif)
+
+    setTimeout(() => wrapper.removeChild(notif), duration)
+
+    btn.addEventListener('click', () => {
+        wrapper.removeChild(notif)
+    },{once: true})
 }
