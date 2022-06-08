196dfdc63045bcedae4369b9a5c18e9135804790
zuramai <saugi.dev@gmail.com>
2022-06-08 09:46:14 +0700

    add: graph algorithm abstract class

--- packages/algorithms/src/algorithms/index.ts
+++ packages/algorithms/src/algorithms/index.ts
@@ -1,1 +1,31 @@
-export * from "./aStar"
+import { NodeInterface } from "graphism";
+import { AlgorithmInterface, ProgressStack, SolveOptions } from "../types";
+import AStarAlgorithm from "./aStar";
+import BFSAlgorithm from "./bfs";
+import DFSAlgorithm from "./dfs";
+import DijkstraAlgorithm from "./dijkstra";
+import KruskalAlgorithm from "./kruskal";
+import PrimAlgorithm from "./prim";
+
+
+const AvailableAlgorithms = {
+    "aStar": AStarAlgorithm,
+    "bfs": BFSAlgorithm,
+    "dfs": DFSAlgorithm,
+    "dijkstra": DijkstraAlgorithm,
+    "prim": PrimAlgorithm,
+    "kruskal": KruskalAlgorithm,
+}
+
+export abstract class GraphAlgorithm implements AlgorithmInterface {
+    nodes: NodeInterface[][] = []
+    path: NodeInterface[] = []
+    startNode: NodeInterface
+    progressStack: ProgressStack[] = []
+
+    abstract solve(solveOptions: SolveOptions): void
+
+    static new<T extends keyof typeof AvailableAlgorithms>(nodes: NodeInterface[][], algorithm: T): GraphAlgorithm {
+        return new AvailableAlgorithms[algorithm](nodes)
+    }
+}
