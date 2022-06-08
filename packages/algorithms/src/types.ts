74e0994da80181b742b8110de0cadd3c61214693
zuramai <saugi.dev@gmail.com>
2022-06-08 09:08:35 +0700

    add: each algorithm types interface

--- packages/algorithms/src/types.ts
+++ packages/algorithms/src/types.ts
@@ -1,15 +1,29 @@
 import { NodeInterface, LineInterface } from "graphism"
 
+export interface ProgressStack {
+    el: NodeInterface | LineInterface
+    status: "PROGRESS" | "CANCEL" | "SUCCESS"
+}
+
 export interface AlgorithmInterface {
     nodes: NodeInterface[][]
     startNode: NodeInterface
-    endNode: NodeInterface
     path: NodeInterface[]
     
-    progressStack: {
-        el: NodeInterface | LineInterface
-        status: "PROGRESS" | "CANCEL" | "SUCCESS"
-    }[]
+    progressStack: ProgressStack[]
 
-    solve: (startNode: NodeInterface, endNode: NodeInterface, speed: number) => void
+    solve: (solveOptions: SolveOptions) => void
+}
+
+export interface ShortestPathAlgorithm {
+    searchValue: string
+    endNode: NodeInterface
+}
+
+export interface SearchAlgorithm {
+    findValue: NodeList
+}
+
+export interface SolveOptions {
+    speed: number
 }
