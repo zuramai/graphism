cf40baa4fd0cbeccb31750a5718b8aab6f1ba9d7
zuramai <saugi.dev@gmail.com>
2022-08-15 16:37:35 +0700

    test: add basic testing

---
+++ packages/graphism/test/algorithms/dijkstra.spec.ts
@@ -0,0 +1,7 @@
+import { describe, test } from "vitest";
+
+describe('dijkstra algorithm', async () => {
+  test('nodes passed correctly', () => {
+
+  })
+})
---
+++ packages/graphism/test/basic.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest'
+import { createGraphism } from './../src'
+
+describe('render graphism', async () => {
+  const el = document.createElement('canvas')
+  const graph = createGraphism({ el })
+
+  test('canvas rendered correctly', () => {
+    expect(graph.canvas).toBeInstanceOf(HTMLCanvasElement)
+  })
+})
