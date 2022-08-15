e659dea203c0e11f832ca1925af62f00ce4c3f43
zuramai <saugi.dev@gmail.com>
2022-08-16 05:39:58 +0700

    chore: lint

--- packages/graphism/test/basic.spec.ts
+++ packages/graphism/test/basic.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'vitest'
 import { createGraphism } from './../src'
 
-describe('render graphism', async () => {
+describe('render graphism', async() => {
   const el = document.createElement('canvas')
   const graph = createGraphism({ el })
 
