4b576f30f68ce5e7b61a027931b4130b84375514
zuramai <ahmad.saugi@binus.ac.id>
2022-12-21 15:14:20 +0700

    feat: form input component

---
+++ playground/src/components/Form/FormSelect.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  data: Record<any, any>
+  name: string
+  label: string
+}>()
+</script>
+
+<template>
+  <div class="form-group">
+    <label :for="name">{{label}}</label>
+    <select id="custom-font-family" :name="name" class="form-input">
+      <option v-for="(value, key) in data" :key="key" :value="key">
+        {{ value }}
+      </option>
+    </select>
+  </div>
+</template>
